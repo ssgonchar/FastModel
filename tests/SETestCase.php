aa59d12bf2bc47b388bb9e554b25acdc4efbc4f6
ssgonchar <fingercrew2@gmail.com>
2016-01-18 10:57:10 +0200

    Make relise version

--- tests/SETestCase.php
+++ tests/SETestCase.php
@@ -19,35 +19,35 @@ abstract class SETestCase extends PHPUnit_Framework_TestCase
              * User roles
              */
             define('ROLE_GUEST', 1);
+
+            /**
+             * Localizations
+             */
+            define('DEFAULT_LANG', 'en');
+
+            /**
+             * DB connection settings
+             */
+            define('APP_DBHOST', 'localhost');
+            define('APP_DBNAME', 'test');
+            define('APP_DBUSER', 'dev');
+            define('APP_DBPASS', '123456');
+            define('DB_TIME_ZONE', '0');
+            define('SLOW_QUERY_TIME', 1); // s
+            define('MAX_LENGTH_PER_PARAM', 100);
+
+            /**
+             * Cache settings
+             */
+            define('CACHE_ENABLED', 'yes');
+            define('MEMCACHE_HOST', 'localhost');
+            define('MEMCACHE_PORT', '11211');
+
+            /**
+             * Log
+             */
+            define('LOG', 'yes');
+            define('APP_LOGS', 'tests\logs\/');
         }
-
-        /**
-         * Localizations
-         */
-        define('DEFAULT_LANG', 'en');
-
-        /**
-         * DB connection settings
-         */
-        define('APP_DBHOST', 'localhost');
-        define('APP_DBNAME', 'test');
-        define('APP_DBUSER', 'dev');
-        define('APP_DBPASS', '123456');
-        define('DB_TIME_ZONE', '0');
-        define('SLOW_QUERY_TIME', 1); // s
-        define('MAX_LENGTH_PER_PARAM', 100);
-
-        /**
-         * Cache settings
-         */
-        define('CACHE_ENABLED', 'yes');
-        define('MEMCACHE_HOST', 'localhost');
-        define('MEMCACHE_PORT', '11211');
-
-        /**
-         * Log
-         */
-        define('LOG', 'yes');
-        define('APP_LOGS', 'tests\logs\/');
     }
 }
